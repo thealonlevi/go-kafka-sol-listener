e0b5b1a77ae3d82cdbcf2cfdce11a9f8140aa003
Alon Levi <levialon@proton.me>
2025-02-01 13:41:56 +0200

    Update

--- scripts/sendToBucket.py
+++ scripts/sendToBucket.py
@@ -28,7 +28,7 @@ def main():
 
     # File IDs (without .json extension)
     file_ids = [
-        "2FjoEdzowSoJEiAoUf1NqAp3ZS2jXrZPy3kt85ZWrRyasC6SVKuen1nRLvM4AWLbWYkw9CJhKZa9RLZpsQV3zY7E", "sQcu476aUzLSYyRYC4Zo5zmdXibEHhVyP4U5onUdbtFHqb12fhqNg1rG241CFtaVFpsXax8TFJ3wc1yB2feqtmN", "RkwYUmP4FvWjJB9AA6fyXgbRpmpcrBm1WpuQnG96cwJxxPVpHMouAGJz3QbT8YoMSR4g5Y3pDq856ZVc8dvB5kY"
+        "Hxp9d3VKmwhywvgbHi6QcmKxoAJXQH8sjaALpffVZM37bCwov9zm5XmWv29Ws1dtuuhKcR7nvdpvSB32txDYFye"
     ] 
 
     if not os.path.exists(matches_directory):
